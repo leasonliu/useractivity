ed370b7e60c30704974b24cf2c04f2f5240fb2e4
eason <yul196@pitt.edu>
2018-04-03 12:40:41 -0400

    tr

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
-var express = require('express'),
-    employees = require('./routes/employees'),
-    app = express();
-
-app.use(express.static('www'));
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + "/www"));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
+app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
 
-app.get('/employees', employees.findAll);
-app.get('/employees/:id', employees.findById);
-app.get('/employees/:id/reports', employees.findReports);
 
 app.set('port', process.env.PORT || 5000);
 
